30fe6781ba74f14e94ea31b7d28ad2f718ae1f81
Ana luz Biolatti <anluz_1420@hotmail.com>
2022-11-01 10:27:09 -0300

    mm

--- src/vistas/Universidadg1.java
+++ src/vistas/Universidadg1.java
@@ -30,7 +30,7 @@ public class Universidadg1 extends javax.swing.JFrame {
         Jpanel = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
+        escritorio = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
@@ -51,13 +51,13 @@ public class Universidadg1 extends javax.swing.JFrame {
 
         jMenu1.setText("Universidad ");
 
-        jMenuItem1.setText("Alumno.");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+        escritorio.setText("Alumno.");
+        escritorio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
+                escritorioActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem1);
+        jMenu1.add(escritorio);
 
         jMenuItem2.setText("Materia.");
         jMenu1.add(jMenuItem2);
@@ -95,9 +95,11 @@ public class Universidadg1 extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+    private void escritorioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_escritorioActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
+        
+        
+    }//GEN-LAST:event_escritorioActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
@@ -140,10 +142,10 @@ public class Universidadg1 extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane Jpanel;
+    private javax.swing.JMenuItem escritorio;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
